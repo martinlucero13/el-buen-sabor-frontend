be70e92b0fae2d9f717d11f3cabcf3f3c4afda20
SantiagoLopezK2 <santilopezk2@gmail.com>
2023-06-22 16:59:42 -0300

    Agregando el Encabezado del Cocinero

---
+++ src/components/Layout/Cocinero/Header/HeaderCocinero.tsx
@@ -0,0 +1,24 @@
+import "./HeaderCocinero.css";
+import { Nav, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+function HeaderCocinero(): JSX.Element {
+    return(
+        <Nav justify variant="tabs" className="header-cocinero">
+            <Nav.Item>
+                <Nav.Link as={Link} to="/cocinero">Pedidos</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <NavDropdown title="Stock" id="stock-dropdown">
+                    <Nav.Link as={Link} to="/cocinero/stock/articuloi">Articulo Insumo</Nav.Link>
+                    <Nav.Link as={Link} to="/cocinero/stock/articulom">Artículo Manufacturado</Nav.Link>
+                </NavDropdown>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/cocinero/rubros">Rubros</Nav.Link>
+            </Nav.Item>
+        </Nav>
+    );
+}
+
+export default HeaderCocinero;
