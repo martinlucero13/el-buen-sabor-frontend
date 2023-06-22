97982aae1f075339defbec0e1e81c46e735d78c0
SantiagoLopezK2 <santilopezk2@gmail.com>
2023-06-22 16:09:38 -0300

    Agregando Cajero Router

---
+++ src/routers/CajeroRouter.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import NavBarEmpleado from "../components/Layout/NavBar/NavBarEmpleado";
+
+function CajeroRouter(): JSX.Element {
+    return(
+        <>
+            <NavBarEmpleado />
+            <Routes>
+            </Routes>
+        </>
+    );
+}
+
+export default CajeroRouter;
