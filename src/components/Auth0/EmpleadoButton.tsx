d7042d77255d7e883cb170f954cd1009e9b7579c
SantiagoLopezK2 <santilopezk2@gmail.com>
2023-06-22 16:48:36 -0300

    Agregando UserButton

---
+++ src/components/Auth0/EmpleadoButton.tsx
@@ -0,0 +1,25 @@
+import "./Button.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+
+function EmpleadoButton(): JSX.Element {
+  const { user, logout } = useAuth0();
+
+  const handleLogout = () => {
+    logout({ logoutParams: { returnTo: window.location.origin } });
+  };
+
+  return (
+    <DropdownButton
+      title={user?.name}
+      variant="bi bi-person-fill btn btn-light"
+      className="navbar-btn"
+    >
+      <Dropdown.Item>Perfil</Dropdown.Item>
+      <Dropdown.Divider />
+      <Dropdown.Item onClick={() => handleLogout()} style={{color: "#F94144"}}>Cerrar Sesión</Dropdown.Item>
+    </DropdownButton>
+  );
+}
+
+export default EmpleadoButton;
---
+++ src/components/Auth0/UserButton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+import "./Button.css";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+
+function UserButton(): JSX.Element {
+  const { user, logout } = useAuth0();
+
+  const handleLogout = () => {
+    logout({ logoutParams: { returnTo: window.location.origin } });
+  };
+
+  return (
+    <DropdownButton
+      title={user?.name}
+      variant="bi bi-person-fill btn btn-light navbar-btn"
+    >
+      <Dropdown.Item>Perfil</Dropdown.Item>
+      <Dropdown.Item>Pedidos</Dropdown.Item>
+      <Dropdown.Divider />
+      <Dropdown.Item onClick={() => handleLogout()} style={{color: "#F94144"}}>Cerrar Sesión</Dropdown.Item>
+    </DropdownButton>
+  );
+}
+
+export default UserButton;
