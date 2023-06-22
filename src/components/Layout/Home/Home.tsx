35a0859d2e52700125d4ef17e1d54fcaa84347ce
SantiagoLopezK2 <santilopezk2@gmail.com>
2023-06-22 17:10:52 -0300

    Agregando Home:EncabezadoHome+Carrusel

---
+++ src/components/Layout/Home/Home.tsx
@@ -0,0 +1,13 @@
+import Slider from "../Slider/Slider";
+import HeaderHome from "../Header/Header";
+
+function Home(): JSX.Element {
+    return(
+        <>
+            <Slider />
+            <HeaderHome />
+        </>
+    );
+}
+
+export default Home;
