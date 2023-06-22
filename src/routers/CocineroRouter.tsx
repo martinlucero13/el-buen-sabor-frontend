2ff68b8db5d069a50906f87e81fc6167f720ad3d
SantiagoLopezK2 <santilopezk2@gmail.com>
2023-06-22 16:08:45 -0300

    Agregando Cocinero Router

---
+++ src/routers/CocineroRouter.tsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import NavBarEmpleado from "../components/Layout/NavBar/NavBarEmpleado";
+import HeaderCocinero from "../components/Layout/Cocinero/Header/HeaderCocinero";
+import TableRubro from "../components/Rubro/TableRubro";
+import TableArticuloManufacturado from "../components/ArticuloManufacturado/TableArticuloManufacturado";
+import { ArticuloManufacturadoAbm } from '../components/ArticuloManufacturado/ArticuloManufacturadoAbm';
+import ArticuloManufacturadoReceta from "../components/ArticuloManufacturado/ArticuloManufacturadoReceta";
+import TableArticuloInsumo from '../components/ArticuloInsumo/TableArticuloInsumo';
+
+function CocineroRouter(): JSX.Element {
+    return(
+        <>
+            <NavBarEmpleado />
+            <HeaderCocinero />
+            <Routes>
+                <Route path="/rubros" element={<TableRubro />} />
+                <Route path="/stock/articulom" element={<TableArticuloManufacturado />} />
+                <Route path="/stock/articulom/abm/:idArticulo" element={<ArticuloManufacturadoAbm />} />
+                <Route path="/stock/articulom/abm/receta/:idArticulo" element={<ArticuloManufacturadoReceta />} />
+                <Route path="/stock/articuloi" element={<TableArticuloInsumo />} />
+            </Routes>
+        </>
+    );
+}
+
+export default CocineroRouter;
