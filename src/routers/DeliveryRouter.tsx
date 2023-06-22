541751cfce9210a8a2698608ae4c9a6b9f97d2b0
SantiagoLopezK2 <santilopezk2@gmail.com>
2023-06-22 16:10:03 -0300

    Agregando Delivery Router

---
+++ src/routers/DeliveryRouter.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import NavBarEmpleado from "../components/Layout/NavBar/NavBarEmpleado";
+
+function DeliveryRouter(): JSX.Element {
+    return(
+        <>
+            <NavBarEmpleado />
+            <Routes>
+            </Routes>
+        </>
+    );
+}
+
+export default DeliveryRouter;
