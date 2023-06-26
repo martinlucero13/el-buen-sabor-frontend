d20b32a511f36d5021ad5a84f711ad71ea02d5a0
SantiagoLopezK2 <santilopezk2@gmail.com>
2023-06-26 19:28:35 -0300

    Aplicando GenericService

--- src/services/ArticuloManufacturadoInsumoService.ts
+++ src/services/ArticuloManufacturadoInsumoService.ts
@@ -1,103 +1,23 @@
-import { URL_API_BASE } from "../constants";
+
 import { ArticuloManufacturadoInsumo } from "../types/ArticuloManufacturadoInsumo";
+import { GenericService } from "./GenericService";
 
 export async function findByArticuloManufacturado(id: number, token: string) {
-    try {
-        const response = await fetch(`${URL_API_BASE}/articulos-manufacturados-insumos/byArticuloManufacturado/${id}`, {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`,
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json() as ArticuloManufacturadoInsumo[];
-        return data;
-    } catch (error) {
-        console.log(error);
-        throw new Error(`Error! ${error}`);
-    }
+    return GenericService<ArticuloManufacturadoInsumo[]>(`articulos-manufacturados-insumos/byArticuloManufacturado/${id}`, "GET", token);
 }
-
+  
 export async function saveArticuloManufacturadoInsumo(articuloManufacturadoInsumo: ArticuloManufacturadoInsumo, token: string) {
-    try {
-        const response = await fetch(`${URL_API_BASE}/articulos-manufacturados-insumos`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify(articuloManufacturadoInsumo),
-        });
-
-        
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-    } catch (error) {
-        console.log(error);
-        throw new Error(`Error! ${error}`);
-    }
+    return GenericService<void>("articulos-manufacturados-insumos", "POST", token, articuloManufacturadoInsumo);
 }
-
+  
 export async function findArticuloManufacturadoInsumoById(id: number, token: string) {
-    try {
-        const response = await fetch(`${URL_API_BASE}/articulos-manufacturados-insumos/${id}`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json() as ArticuloManufacturadoInsumo;
-
-        return data;
-    } catch (error) {
-        console.log(error);
-        throw new Error(`Error! ${error}`);
-    }
+    return GenericService<ArticuloManufacturadoInsumo>(`articulos-manufacturados-insumos/${id}`, "GET", token);
 }
-
+  
 export async function updateArticuloManufacturadoInsumo(id: number, articuloManufacturadoInsumo: ArticuloManufacturadoInsumo, token: string) {
-    try {
-        const response = await fetch(`${URL_API_BASE}/articulos-manufacturados-insumos/${id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify(articuloManufacturadoInsumo),
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-    } catch (error) {
-        console.log(error);
-        throw new Error(`Error! ${error}`);
-    }
+    return GenericService<void>(`articulos-manufacturados-insumos/${id}`, "PUT", token, articuloManufacturadoInsumo);
 }
-
+  
 export async function deleteArticuloManufacturadoInsumo(id: number, token: string) {
-    try {
-        const response = await fetch(`${URL_API_BASE}/articulos-manufacturados-insumos/${id}`, {
-            method: 'DELETE',
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-    } catch (error) {
-        console.log(error);
-        throw new Error(`Error! ${error}`);
-    }
+    return GenericService<void>(`articulos-manufacturados-insumos/${id}`, "DELETE", token);
 }
