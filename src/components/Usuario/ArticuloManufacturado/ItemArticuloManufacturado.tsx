fcd859cdb39a75f8f43d47a41f7d8d3e3e9fd294
SantiagoLopezK2 <santilopezk2@gmail.com>
2023-06-28 18:47:27 -0300

    Modificando para que aparezca el button al pasar el mouse

--- src/components/Usuario/ArticuloManufacturado/ItemArticuloManufacturado.tsx
+++ src/components/Usuario/ArticuloManufacturado/ItemArticuloManufacturado.tsx
@@ -2,30 +2,49 @@ import { Card } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import { ArticuloManufacturado } from "../../../types/ArticuloManufacturado"
 import { Button } from 'react-bootstrap';
-import { useContext } from "react";
+import { useContext,useState } from "react";
 import { CarritoContext } from "../../Context/CarritoContext";
-
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './ArticuloManufacturado.css';
 
 export const ItemArticuloManufacturado = (props: ArticuloManufacturado) => {
     const { agregarAlCarrito } = useContext(CarritoContext);
 
+    const [mostrarBoton, setMostrarBoton] = useState(false);
+
+    const handleMouseEnter = () => {
+      setMostrarBoton(true);
+    };
+  
+    const handleMouseLeave = () => {
+      setMostrarBoton(false);
+    };
+  
 
   return (
     <Card
       style={{ width: '18rem' }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <Link to={`/detalle/${props.id}`}>
         <Card.Img
           src={props.imagen.imagenUrl}
           alt={props.denominacion}
-          className="maxAltoImg"
+          className="maxAltoImg-item"
           variant="top"
         />
       </Link>
       <Card.Body>
         <Card.Title>{props.denominacion}</Card.Title>
         <Card.Text>${props.articuloManufacturadoPrecioVenta.precioVenta}</Card.Text>
-        <Button onClick={() => agregarAlCarrito(props)}>Agregar al carrito</Button>
+        <TransitionGroup>
+          {mostrarBoton && (
+            <CSSTransition classNames="fade" timeout={300}>
+              <Button onClick={() => agregarAlCarrito(props)}>Agregar al carrito</Button>
+            </CSSTransition>
+          )}
+        </TransitionGroup>
       </Card.Body>
     </Card>
   );
