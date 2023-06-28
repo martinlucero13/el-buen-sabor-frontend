dd412dd468758c1537dd7168aab7b785ad8dd9b0
SantiagoLopezK2 <santilopezk2@gmail.com>
2023-06-28 01:23:04 -0300

    Agregando a UserButton que pueda ir a perfil

--- src/components/Auth0/UserButton.tsx
+++ src/components/Auth0/UserButton.tsx
@@ -3,12 +3,18 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 import "./Button.css";
 import { Dropdown, DropdownButton } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function UserButton(): JSX.Element {
   const { user, logout } = useAuth0();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     logout({ logoutParams: { returnTo: window.location.origin } });
+  };
+
+  const navigateToProfile = () => {
+    navigate("/perfil"); 
   };
 
   return (
@@ -16,7 +22,7 @@ function UserButton(): JSX.Element {
       title={user?.name}
       variant="bi bi-person-fill btn btn-light navbar-btn"
     >
-      <Dropdown.Item>Perfil</Dropdown.Item>
+      <Dropdown.Item onClick={navigateToProfile}>Perfil</Dropdown.Item>
       <Dropdown.Item>Pedidos</Dropdown.Item>
       <Dropdown.Divider />
       <Dropdown.Item onClick={() => handleLogout()} style={{color: "#F94144"}}>Cerrar Sesión</Dropdown.Item>
